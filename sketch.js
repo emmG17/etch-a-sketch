e6adeb4ba455348eac3baa748cde4832f0537bc8
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-15 17:33:08 -0600

    Add getPixels to generate arbitrary number of pixels on demand

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 // Create 16x16 pixel divs
 let n_rows = 16;
 let n_cols = 16;
-
 let n_pixels = n_rows * n_cols;
-let pixels = [];
 
-for (let i = 0; i < n_pixels; i++) {
-    const pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    pixels.push(pixel);
+function getPixels(n_pixels) {
+    let pixels = [];
+
+    for (let i = 0; i < n_pixels; i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixels.push(pixel);
+    }
+
+    return pixels
 }
+
+const pixels = getPixels(n_pixels);
 
 // Append pixels to the grid
 const pixelGrid = document.querySelector(".grid-container");
