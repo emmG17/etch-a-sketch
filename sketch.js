c66f2a82f85f6b6da836d9943351c8eeaddffa76
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-16 00:17:55 -0600

    Add inputs to allow resize the grid

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 // Create 16x16 pixel divs
-let n_rows = 16;
-let n_cols = 16;
-let n_pixels = n_rows * n_cols;
+let nCols = 16;
+let nRows = 16;
 const pixelGrid = document.querySelector(".grid-container");
 const resetButton = document.getElementById("reset");
+const createGridButton = document.getElementById("create-grid");
+const colsInput = document.getElementById("n-cols");
+const rowsInput = document.getElementById("n-rows")
 
 function defineGrid(grid, n_rows, n_cols) {
     grid.style.gridTemplateRows = `repeat(${n_rows}, 1fr)`;
@@ -43,11 +45,33 @@ function populateGrid(grid, n_pixels) {
     }
 }
 
-resetButton.addEventListener("click", () => {
+function createGrid(n_rows, n_cols){
+    let n_pixels = n_rows * n_cols;
     pixelGrid.innerHTML = "";
     defineGrid(pixelGrid, n_rows, n_cols);
     populateGrid(pixelGrid, n_pixels);
-})
+}
 
-defineGrid(pixelGrid, n_rows, n_cols);
-populateGrid(pixelGrid, n_pixels);
+resetButton.addEventListener("click", () => {
+    createGrid(nCols, nRows);
+});
+
+
+createGridButton.addEventListener("click", () => {
+    let cols = colsInput.valueAsNumber;
+    let rows = rowsInput.valueAsNumber;
+    let isValidInput = (rows && cols) && (colsInput.validity.valid && rowsInput.validity.valid)
+    if (isValidInput)  {
+        nRows = rows;
+        nCols = cols;
+    }
+
+    pixelGrid.innerHTML = "";
+    defineGrid(pixelGrid, nRows, nCols);
+    populateGrid(pixelGrid, nRows*nCols);
+});
+
+colsInput.setAttribute("placeholder", nCols.toString());
+rowsInput.setAttribute("placeholder", nRows.toString());
+
+createGrid(nRows, nCols);
